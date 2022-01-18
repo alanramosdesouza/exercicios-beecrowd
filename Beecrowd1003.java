d4fb85b21b16197767e8e487989cfa2a3ec6c12c
AlanRamosS <alanramosdesouza@gmail.com>
2022-01-18 09:33:00 -0300

    Soma simples

---
+++ Beecrowd1003.java
@@ -0,0 +1,18 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class Beecrowd1003 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, SOMA;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		SOMA = A + B;
+		System.out.println("SOMA = " + SOMA);
+
+	}
+
+}
